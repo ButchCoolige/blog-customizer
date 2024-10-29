1e36855eca32ede1f9d7c87b1ac2ca19f0728ac6
Ilya Balashov <butch-c@yandex.ru>
2024-10-29 22:20:05 +0300

    Side menu opens

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,12 +3,21 @@ import { Button } from 'src/ui/button';
 
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const handleOpenForm = () => {
+		setOpen((prevOpen) => !prevOpen);
+	};
+
+	const modalStyle = clsx(styles.container, { [styles.container_open]: open });
+
 	return (
 		<>
-			<ArrowButton isOpen={true} onClick={() => {}} />
-			<aside className={clsx(styles.container, styles.container_open)}>
+			<ArrowButton isOpen={true} onClick={handleOpenForm} />
+			<aside className={modalStyle}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
